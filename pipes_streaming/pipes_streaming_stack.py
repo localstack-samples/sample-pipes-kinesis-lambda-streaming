bb39517c6ae263aae30416bab143fe924193bd53
HarshCasper <erbeusgriffincasper@gmail.com>
2024-06-03 14:32:27 +0530

    simplify IAM code

--- pipes_streaming/pipes_streaming_stack.py
+++ pipes_streaming/pipes_streaming_stack.py
@@ -15,9 +15,11 @@ from aws_cdk.aws_lambda import InlineCode
 THIS_FOLDER = os.path.dirname(os.path.realpath(__file__))
 TEST_LAMBDA_PYTHON_ENRICHMENT = os.path.join(THIS_FOLDER, "functions/enrichment.py")
 
+
 def load_file(path):
-    with open(path, 'r') as file:
+    with open(path, "r") as file:
         return file.read()
+
 
 class PipesStreamingStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
@@ -41,35 +43,10 @@ class PipesStreamingStack(Stack):
         pipe_role = iam.Role(
             self, "PipeRole", assumed_by=iam.ServicePrincipal("pipes.amazonaws.com")
         )
-        source_policy = iam.PolicyStatement(
-            sid="AllowSourceStream",
-            effect=iam.Effect.ALLOW,
-            actions=[
-                "kinesis:DescribeStream",
-                "kinesis:DescribeStreamSummary",
-                "kinesis:GetRecords",
-                "kinesis:GetShardIterator",
-                "kinesis:ListStreams",
-                "kinesis:ListShards",
-                "sqs:SendMessage",
-            ],
-            resources=[source_stream.stream_arn, dlq.queue_arn],
-        )
-        pipe_role.add_to_policy(source_policy)
-        enrichment_policy = iam.PolicyStatement(
-            sid="AllowEnrichmentLambda",
-            effect=iam.Effect.ALLOW,
-            actions=["lambda:InvokeFunction"],
-            resources=[enrichment_function.function_arn],
-        )
-        pipe_role.add_to_policy(enrichment_policy)
-        target_policy = iam.PolicyStatement(
-            sid="AllowTargetStream",
-            effect=iam.Effect.ALLOW,
-            actions=["kinesis:PutRecord", "kinesis:PutRecords"],
-            resources=[target_stream.stream_arn],
-        )
-        pipe_role.add_to_policy(target_policy)
+        source_stream.grant_read(pipe_role)
+        dlq.grant_send_messages(pipe_role)
+        enrichment_function.grant_invoke(pipe_role)
+        target_stream.grant_write(pipe_role)
 
         # Pipe
         pipe = pipes.CfnPipe(
@@ -80,7 +57,9 @@ class PipesStreamingStack(Stack):
             source_parameters=pipes.CfnPipe.PipeSourceParametersProperty(
                 kinesis_stream_parameters=pipes.CfnPipe.PipeSourceKinesisStreamParametersProperty(
                     starting_position="TRIM_HORIZON",
-                    dead_letter_config=pipes.CfnPipe.DeadLetterConfigProperty(arn=dlq.queue_arn),
+                    dead_letter_config=pipes.CfnPipe.DeadLetterConfigProperty(
+                        arn=dlq.queue_arn
+                    ),
                     maximum_retry_attempts=1,
                     batch_size=2,
                 ),
@@ -99,6 +78,8 @@ class PipesStreamingStack(Stack):
         cdk.CfnOutput(self, "SourceStreamName", value=source_stream.stream_name)
         cdk.CfnOutput(self, "DlqUrl", value=dlq.queue_url)
         cdk.CfnOutput(self, "DlqName", value=dlq.queue_name)
-        cdk.CfnOutput(self, "EnrichmentFunctionName", value=enrichment_function.function_name)
+        cdk.CfnOutput(
+            self, "EnrichmentFunctionName", value=enrichment_function.function_name
+        )
         cdk.CfnOutput(self, "TargetStreamName", value=target_stream.stream_name)
         cdk.CfnOutput(self, "RoleName", value=pipe_role.role_name)
